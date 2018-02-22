0006c3653fc5c30be7207a5778d6d1b84440c49c
christiankiller <christian.killer@uzh.ch>
2018-02-22 14:22:42 +0100

    provotum/eth-private-net#4: Generate & test privatekeys.json

---
+++ steps/01-generate-keypairs/clean.sh
@@ -0,0 +1,3 @@
+rm -rf keystore/*
+rm genesis.json
+rm privatekeys.json
--- steps/01-generate-keypairs/generateKeys.js
+++ steps/01-generate-keypairs/generateKeys.js
@@ -9,36 +9,44 @@ generateKeys();
 
 
 function generateKeys() {
+
     if (process.env.NUMBER_OF_KEYS == undefined) {
         console.log('\x1b[31m', 'Something went wrong with dotenv .env', '\x1b[0m');
         return;
     }
 
-    var addressArray= new Array;
-    for (var i = 0; i < process.env.NUMBER_OF_KEYS; i++) {
-
+    var addressArray = new Array;
+    var privateKeysArray = new Array;
+    for (var i = 1; i <= process.env.NUMBER_OF_KEYS; i++) {
         console.log('\x1b[32m', "Generating [" + i + "/" + process.env.NUMBER_OF_KEYS + "] keys", '\x1b[0m');
         var dk = keythereum.create();
         var readableAddress = keythereum.privateKeyToAddress(dk.privateKey);
-
-        console.log("readableAddress" + readableAddress);
-        console.log("privateKeyHex:" + dk.privateKey.toString('hex'));
         addressArray.push(readableAddress);
+        privateKeysArray.push(dk.privateKey.toString('hex'));
 
-        var keyObject = keythereum.dump(password, dk.privateKey, dk.salt, dk.iv);
-        keythereum.exportToFile(keyObject);
+        //var keyObject = keythereum.dump(password, dk.privateKey, dk.salt, dk.iv);
+        //keythereum.exportToFile(keyObject);
     }
 
+    generateJSONForIdentityProvider(privateKeysArray);
     generateGenesisBlock(addressArray);
 
 }
+
+function generateJSONForIdentityProvider(privateKeysArray) {
+    var privateKeyObj = {};
+    for (var i = 0; i < privateKeysArray.length; i++) {
+        privateKeyObj[i] = privateKeysArray[i];
+    }
+    var privateKeyJson = JSON.stringify(privateKeyObj);
+    fs.writeFile('privatekeys.json', privateKeyJson);
+}
+
+
 function generateGenesisBlock(addressArray) {
-
     console.log('\x1b[32m', "Generating genesis block.", '\x1b[0m');
-
-    console.log(addressArray);
-
     var genesisObj = {};
+
     genesisObj.nonce = process.env.GENESIS_NONCE;
     genesisObj.mixhash = process.env.GENESIS_MIXHASH;
     genesisObj.difficulty = process.env.GENESIS_NONCE
@@ -63,22 +71,14 @@ function generateGenesisBlock(addressArray) {
 
 }
 
-function createPreAllocObject(addressArray){
-    var length = addressArray.length;
+function createPreAllocObject(addressArray) {
     var balanceObj = {};
     balanceObj.balance = process.env.GENESIS_ALLOC_BALANCE;
     var preAllocObject = {};
 
-    for(var i = 0; i<length; i++){
+    for (var i = 0; i < addressArray.length; i++) {
         var currentAddress = addressArray[i];
         preAllocObject[currentAddress] = balanceObj;
-        //DEBUG console.log("preAllocObject)"+JSON.stringify(preAllocObject));
     }
     return preAllocObject;
 }
-
-// Now as we have all the addresses
-// TODO Generate 100 private keys and save them to a JSON according to the mock-identity provider format
-// TODO Then, run the private network (eth-private-net) with the genesis file that was just generated
-// TODO Add eth-private net as submodule?
-// TODO This concludes step 1, the proxy contract can now be deployed --> Step 2
