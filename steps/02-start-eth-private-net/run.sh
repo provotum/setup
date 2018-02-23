0d24d162ef6a2323b1bb54cc3eadc9023f7ca6e5
christiankiller <christian.killer@uzh.ch>
2018-02-23 15:42:31 +0100

    provotum/setup#1: Add step 2 for private net

---
+++ steps/02-start-eth-private-net/run.sh
@@ -0,0 +1,11 @@
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+NORMAL=$(tput sgr0)
+YELLOW=$(tput setaf 3)
+
+# calling key generation with node
+printf "${YELLOW} %s\n Starting 5 nodes for a private network \n${NORMAL}"
+
+# TODO 
+
+printf "${NORMAL}"
