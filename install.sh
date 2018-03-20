b829e9afbed8bec8e7dd41ed039f391655f96070
christiankiller <christian.killer@uzh.ch>
2018-03-20 20:37:35 +0100

    Fix path typo in install script

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ git submodule init && git submodule update
 (cd $(pwd)/resources/poa-private-net && git submodule init);
 (cd $(pwd)/resources/poa-private-net && git submodule update);
 
-(cd $(pwd)/steps/01-generate-keypairs/src && npm install);
+(cd $(pwd)/steps/01-deploy-poa-net/src && npm install);
